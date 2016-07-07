0ad5b354056f1215efe443f9e75695ad4750b34c
Mr. Piggy <mcpepig123@gmail.com>
2016-07-06 17:01:22 -0700

    Um

--- src/LaunchPads/Main.php
+++ src/LaunchPads/Main.php
@@ -7,7 +7,7 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase{
     public function onEnable(){
-        @mkdir($this->getServer()->getDataPath() . "/plugins/LaunchPads/");
+        @mkdir($this->getDataFolder());
         $this->launchpads = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
             "launchpad" => 152,
             "enable-torch-mode" => true
