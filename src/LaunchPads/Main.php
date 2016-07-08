c8b8ae5436715f061877251365c8b020fe81467e
MCPEPIG <mcpepig123@gmail.com>
2016-07-08 12:29:02 -0700

    Multiple launchpads!

--- src/LaunchPads/Main.php
+++ src/LaunchPads/Main.php
@@ -7,11 +7,7 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase{
     public function onEnable(){
-        @mkdir($this->getDataFolder());
-        $this->launchpads = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
-            "launchpad" => 152,
-            "enable-torch-mode" => true
-        ));
+		$this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info("§aEnabled.");
     }
