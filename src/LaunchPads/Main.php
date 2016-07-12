3a6541ad25d9cf621d2c57ce72a4c4b6a71af232
Mr. Piggy <mcpepig123@gmail.com>
2016-07-12 15:38:09 -0700

    Fixes formatting

--- src/LaunchPads/Main.php
+++ src/LaunchPads/Main.php
@@ -7,7 +7,7 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase{
     public function onEnable(){
-		$this->saveDefaultConfig();
+    	$this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info("§aEnabled.");
     }
