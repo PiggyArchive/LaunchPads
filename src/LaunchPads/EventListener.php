275dd05176eda032c64ce81f5f5c66bf951f82f8
Mr. Piggy <mcpepig123@gmail.com>
2016-07-13 11:52:23 -0700

    Better Formatting :P

--- src/LaunchPads/EventListener.php
+++ src/LaunchPads/EventListener.php
@@ -1,24 +1,23 @@
 <?php
-
 namespace LaunchPads;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 
-class EventListener implements Listener{
-    public function __construct($plugin){
+class EventListener implements Listener {
+    public function __construct($plugin) {
         $this->plugin = $plugin;
     }
 
-    public function onMove(PlayerMoveEvent $event){
+    public function onMove(PlayerMoveEvent $event) {
         $player = $event->getPlayer();
         $block = $player->getLevel()->getBlock($player->floor()->subtract(0, 1));
         $block2 = $player->getLevel()->getBlock($player->floor()->subtract(0, 2));
         $launchpads = $this->plugin->getconfig()->get("launchpad");
-        if(in_array($block->getId(), $launchpads)){
-            if($this->plugin->getconfig()->get("enable-torch-mode")){
-                if($block2->getId() == 50){
-                    switch($block2->getDamage()){
+        if(in_array($block->getId(), $launchpads)) {
+            if($this->plugin->getconfig()->get("enable-torch-mode")) {
+                if($block2->getId() == 50) {
+                    switch($block2->getDamage()) {
                         case 2:
                             $player->knockBack($player, 0, -1, 0, 1);
                             return true;
@@ -32,9 +31,9 @@ class EventListener implements Listener{
                             $player->knockBack($player, 0, 0, 1, 1);
                             return true;
                     }
-                }   
+                }
             }
-            switch($player->getDirection()){
+            switch($player->getDirection()) {
                 case 0:
                     $player->knockBack($player, 0, 1, 0, 1);
                     return true;
@@ -47,7 +46,7 @@ class EventListener implements Listener{
                 case 3:
                     $player->knockBack($player, 0, 0, -1, 1);
                     return true;
-            }    
+            }
         }
     }
 }
--- src/LaunchPads/Main.php
+++ src/LaunchPads/Main.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace LaunchPads;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase{
-    public function onEnable(){
-    	$this->saveDefaultConfig();
+class Main extends PluginBase {
+    public function onEnable() {
+        $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getLogger()->info("§aEnabled.");
     }
